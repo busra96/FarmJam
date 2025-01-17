642780258b736e46aaddc678f36bf25984589a9c
busrasimsek <busra.simsek@lootcopter.com>
2025-01-17 18:19:52 +0300

    movement

--- FarmJam/Assets/FarmJam/Scripts/EmptyBox/EmptyBoxMovement.cs
+++ FarmJam/Assets/FarmJam/Scripts/EmptyBox/EmptyBoxMovement.cs
@@ -1,102 +1,123 @@
-using System;
 using DG.Tweening;
 using UnityEngine;
 
 public class EmptyBoxMovement : MonoBehaviour
 {
-    public Transform RotatePoint;
-    public Camera mainCamera;
-    public LayerMask planeLayer;
-    public float MaxYPos = 1.25f;
-    public float moveSpeed = 5f; // Objenin hareket hızı
-    
-    private bool _isMouseDown = false;
-    private float _defaultTimer = .5f;
-    private float _timer;
-    
-    private Ray _currentRay; // Ray'i saklamak için bir değişken
-    private bool _isRayValid = false; // Ray'in geçerli olup olmadığını kontrol etmek için
-    private Vector3 _startPos;
-    
-    private Vector3 _targetPos; // Objenin hedef pozisyonu (mouse'un işaret ettiği yer)
-    private Tween ScaleTween;
+    [Header("References")]
+    [SerializeField] private Transform rotatePoint;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private LayerMask planeLayer;
+
+    [Header("Settings")]
+    [SerializeField] private float maxYPos = 1.25f;
+    [SerializeField] private float moveSpeed = 5f; 
+    [SerializeField] private float clickTimerDuration = 0.25f;
+    [SerializeField] private Vector3 initialScale = Vector3.one * 0.8f;
+    [SerializeField] private Vector3 clickScale = Vector3.one;
+
+    private bool isMouseDown;
+    private float clickTimer;
+    private Vector3 startPosition;
+    private Vector3 movingPosition;
+    private Vector3 lastMouseWorldPosition;
+    private Tween scaleTween;
 
     private void Start()
     {
-        _startPos = transform.position;
-        _targetPos = _startPos;
-        transform.localScale = Vector3.one * .8f;
+        InitializeValues();
     }
 
-    void Update()
+    private void Update()
     {
-        if (_isMouseDown)
-        {
-            _timer -= Time.deltaTime;
-            if (_timer <= 0)
-            {
-                _timer = 0;
-                _isMouseDown = false;
-            }
-        }
+        HandleMouseDown();
+        HandleMouseHold();
+        HandleMouseUp();
+    }
 
+    private void InitializeValues()
+    {
+        startPosition = transform.position;
+        movingPosition = startPosition;
+        transform.localScale = initialScale;
+    }
+
+    private void HandleMouseDown()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            _isMouseDown = true;
-            _timer = _defaultTimer;
-            if(ScaleTween != null) ScaleTween.Kill();
-            ScaleTween = transform.DOScale(Vector3.one, .25f).SetEase(Ease.Linear);
-        }
-        
-        if (Input.GetMouseButton(0))
-        {
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            _currentRay = ray; // Ray'i kaydediyoruz
-            _isRayValid = true;
-          
-          
-            if (Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity, planeLayer))
-            {
-                Vector3 hitPoint = hitInfo.point;
-                _targetPos  = new Vector3(hitPoint.x, MaxYPos, hitPoint.z);
-            }
-        }
-        else
-        {
-            _isRayValid = false; // Ray'i gizlemek için geçersiz yapıyoruz
-        }
+            isMouseDown = true;
+            clickTimer = clickTimerDuration;
 
-        transform.position = Vector3.MoveTowards(transform.position, _targetPos, moveSpeed * Time.deltaTime);
-        
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (_isMouseDown && _timer > 0)
-            {
-                _timer = 0;
-                _isMouseDown = false;
-                
-                float yPos = RotatePoint.eulerAngles.y;
-                Debug.Log("  ypos 0 " + yPos);
-                yPos += 90;
-                Debug.Log("  ypos " + yPos);
-                RotatePoint.DORotateQuaternion( Quaternion.Euler(new Vector3(0, yPos, 0)), 0.1f).SetEase(Ease.Linear);
-            }
+            ScaleTweenTo(clickScale, 0.25f);
 
-            // Objeyi başlangıç pozisyonuna geri döndürme
-            _targetPos = _startPos;
-            transform.DOMove(_startPos, .1f).SetEase(Ease.Linear);
-            if(ScaleTween != null) ScaleTween.Kill();
-            ScaleTween = transform.DOScale(Vector3.one * .8f, .1f).SetEase(Ease.Linear);
+            movingPosition = transform.position;
+            lastMouseWorldPosition = GetMouseWorldPosition();
         }
     }
-    
-    // Gizmos çizimi
-    private void OnDrawGizmos()
+
+    private void HandleMouseHold()
     {
-        if (_isRayValid)
+        if (!Input.GetMouseButton(0)) return;
+
+        clickTimer = Mathf.Max(clickTimer - Time.deltaTime, 0);
+
+        Vector3 currentMouseWorldPos = GetMouseWorldPosition();
+
+        Vector3 velocity = currentMouseWorldPos - lastMouseWorldPosition;
+        velocity.y = 0; // Sadece x ve z eksenlerinde hareket
+
+        movingPosition += velocity;
+
+        Vector3 targetPos = new Vector3(movingPosition.x, maxYPos, movingPosition.z);
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        lastMouseWorldPosition = currentMouseWorldPos;
+    }
+
+    private void HandleMouseUp()
+    {
+        if (!Input.GetMouseButtonUp(0)) return;
+
+        if (isMouseDown && clickTimer > 0)
         {
-            Gizmos.color = Color.red; // Ray'in rengini kırmızı yapıyoruz
-            Gizmos.DrawRay(_currentRay.origin, _currentRay.direction * 100); // Ray'i çiziyoruz
+            RotateObject();
         }
+
+        ResetObjectPositionAndScale();
+    }
+
+    private void RotateObject()
+    {
+        isMouseDown = false;
+
+        float currentYRotation = rotatePoint.eulerAngles.y;
+        float newYRotation = currentYRotation + 90f;
+
+        rotatePoint.DORotateQuaternion(Quaternion.Euler(0, newYRotation, 0), 0.1f).SetEase(Ease.Linear);
+    }
+
+    private void ResetObjectPositionAndScale()
+    {
+        isMouseDown = false;
+        transform.DOMove(startPosition, 0.1f).SetEase(Ease.Linear);
+        ScaleTweenTo(initialScale, 0.1f);
+    }
+
+    private void ScaleTweenTo(Vector3 targetScale, float duration)
+    {
+        scaleTween?.Kill();
+        scaleTween = transform.DOScale(targetScale, duration).SetEase(Ease.Linear);
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity, planeLayer))
+        {
+            return hitInfo.point;
+        }
+
+        return lastMouseWorldPosition; // Eğer bir yere vurmazsa son pozisyonu döndür
     }
 }
