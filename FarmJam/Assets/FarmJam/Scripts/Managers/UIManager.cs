ece7c73c4e124274f3377ece5a69ebf33433fa85
busrasimsek <busra.simsek@lootcopter.com>
2025-11-16 03:02:49 +0300

    updated ui panels

--- FarmJam/Assets/FarmJam/Scripts/Managers/UIManager.cs
+++ FarmJam/Assets/FarmJam/Scripts/Managers/UIManager.cs
@@ -1,18 +1,24 @@
 using Signals;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject WinPanel;
     public GameObject FailPanel;
     public GameObject GameplayPanel;
+    public Button NextLevelButton;
+    public Button RetryLevelButton;
     
     public void Init()
     {
         UISignals.OnWinPanelActive.AddListener(OnWinPanelActive);
         UISignals.OnFailPanelActive.AddListener(OnFailPanelActive);
         UISignals.OnGameplayPanelActive.AddListener(OnGameplayPanelActive);
+        NextLevelButton.onClick.AddListener(LoadNextLevel);
+        RetryLevelButton.onClick.AddListener(LoadCurrentLevel);
     }
+
     public void Disable()
     {
         UISignals.OnWinPanelActive.RemoveListener(OnWinPanelActive);
@@ -37,5 +43,13 @@ public class UIManager : MonoBehaviour
     {
         GameplayPanel.SetActive(false);
         WinPanel.SetActive(true);
+    }
+    
+    private void LoadCurrentLevel()
+    {
+    }
+
+    private void LoadNextLevel()
+    {
     }
 }
