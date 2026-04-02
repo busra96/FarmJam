92df0f6169769edc04cbfcab43a9764dc46c99b2
busrasimsek <busrasimsek929@gmail.com>
2026-04-02 17:56:55 +0300

    add input manager

---
+++ FarmJam/Assets/FarmBlast(LikeEggBlast)/Scripts/Managers/InputManager.cs
@@ -0,0 +1,36 @@
+using Signals;
+using UnityEngine;
+
+namespace FarmBlast
+{
+    public class InputManager : MonoBehaviour
+    {
+        private bool isActive;
+
+        public void Init()
+        {
+            isActive = true;
+        }
+
+        private void Update()
+        {
+            if (!isActive) return;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                InputSignals.OnInputGetMouseDown?.Dispatch(Input.mousePosition);
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                InputSignals.OnInputGetMouseHold?.Dispatch(Input.mousePosition);
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                InputSignals.OnInputGetMouseUp?.Dispatch(Input.mousePosition);
+            }
+        }
+
+        public void DisableInput() => isActive = false;
+    }
+
+}
